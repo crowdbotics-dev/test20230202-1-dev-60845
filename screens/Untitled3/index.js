297b1398623587592af97622acbc5ea6a130d46b
daniel palacios <daniel.palacios@crowdbotics.com>
2023-02-02 12:36:26 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,3 +1,4 @@
+import { View } from "react-native";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
@@ -8,13 +9,92 @@ const Untitled3 = () => {
       padding: 10,
       position: 'relative',
       flex: 1
-    }}></ScrollView>
+    }}><View style={styles.RUuklDCQ}></View><View style={styles.cCZaiNTD}><View style={styles.eXKOxPRH}></View><View style={styles.XgIaxlCF}><View style={styles.fnthCqtD}></View><View style={styles.VIVmecXy}></View><View style={styles.hhmsvHWk}></View></View></View><View style={styles.XGGEiPrk}></View></ScrollView>
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: '100%'
+  },
+  RUuklDCQ: {
+    flex: 1,
+    backgroundColor: "#ed8080"
+  },
+  ViSlIsER: {
+    position: "absolute",
+    width: 344,
+    height: 559,
+    left: 0,
+    top: -3
+  },
+  AQqNqLYt: {
+    width: 92,
+    height: 60,
+    backgroundColor: "#FFFFFF"
+  },
+  YFBRPbwy: {
+    backgroundColor: "#000000",
+    height: 1
+  },
+  KtqFNAHW: {
+    flex: 1
+  },
+  vYzqdKOY: {
+    flex: 1
+  },
+  lptumvdg: {
+    flex: 1
+  },
+  RTdCRdST: {
+    flex: 1
+  },
+  SErQzRBE: {
+    position: "absolute",
+    width: 339,
+    height: 544,
+    left: 0,
+    top: 0
+  },
+  nkEddmyc: {
+    width: 92,
+    height: 60,
+    backgroundColor: "#FFFFFF"
+  },
+  ykqbDhuy: {
+    backgroundColor: "#000000",
+    height: 1
+  },
+  cROeYEEu: {
+    width: 330,
+    height: 62
+  },
+  cCZaiNTD: {
+    flex: 1,
+    backgroundColor: "#c0cc81",
+    flexDirection: "row"
+  },
+  XGGEiPrk: {
+    flex: 1,
+    backgroundColor: "#caf1ba"
+  },
+  eXKOxPRH: {
+    flex: 1
+  },
+  XgIaxlCF: {
+    flex: 1
+  },
+  fnthCqtD: {
+    flex: 1,
+    backgroundColor: "#5dcaed"
+  },
+  VIVmecXy: {
+    flex: 1,
+    backgroundColor: "#e5ab7d"
+  },
+  hhmsvHWk: {
+    flex: 1,
+    backgroundColor: "#f3ade5"
   }
 });
 export default Untitled3;
---
+++ screens/Untitled4/index.js
@@ -0,0 +1,75 @@
+import { Pressable } from "react-native";
+import { Text } from "react-native";
+import { ImageBackground } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled4 = ({
+  navigation
+}) => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Pressable onPress={() => navigation.navigation.navigate("Untitled5", {})}><View style={styles.elCfXaDw}><Text style={styles.UvPTCBzZ}>START</Text></View></Pressable><ImageBackground style={styles.vhMQtaAO} source={require("./escudo.png")} resizeMode="cover"></ImageBackground></ScrollView>
+    <View style={styles.XqGQWGyw}><Text style={styles.znecCjPq}>EXIT</Text></View></SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  elCfXaDw: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    left: 20,
+    top: 472
+  },
+  XqGQWGyw: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    left: 180,
+    top: 480
+  },
+  vhMQtaAO: {
+    width: 319,
+    height: 377,
+    position: "absolute",
+    left: 19,
+    top: 26
+  },
+  UvPTCBzZ: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 18,
+    borderRadius: 0,
+    position: "absolute",
+    left: 40,
+    top: 20,
+    fontWeight: "900",
+    color: "#0e6ee9"
+  },
+  znecCjPq: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    position: "absolute",
+    left: 48,
+    top: 21,
+    fontWeight: "900"
+  }
+});
+export default Untitled4;
